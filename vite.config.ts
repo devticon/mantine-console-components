b52c935251f7e0267d42c00b4dff51cf7f253574
Dariusz Syncerek <dsyncerek@outlook.com>
2023-10-04 18:20:20 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
         '@mantine/core',
         '@mantine/dates',
         '@mantine/hooks',
+        '@mantine/notifications',
         '@remix-run/react',
         'dayjs',
         'zod',
