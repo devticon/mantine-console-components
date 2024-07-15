d06992e6ac584c0ad7390b21f9869a5e193a0fa9
Dariusz Syncerek <dsyncerek@outlook.com>
2024-07-15 13:20:21 +0200

    [ch7347] fix date range

--- lib/components/Table/TableFilter.tsx
+++ lib/components/Table/TableFilter.tsx
@@ -1,9 +1,7 @@
 import { Group, MultiSelect, NumberInput, Select, Switch, TextInput } from '@mantine/core';
-import { DatePickerInput, DateTimePicker, DateValue } from '@mantine/dates';
-import type { DatePickerValue } from '@mantine/dates/lib/types/DatePickerValue';
+import { DatePickerInput, DateTimePicker } from '@mantine/dates';
 import dayjs from 'dayjs';
 import type { ComponentProps, ReactNode } from 'react';
-import { useState } from 'react';
 import type { IconType } from 'react-icons';
 import invariant from 'tiny-invariant';
 import { parseDate } from '../../utils/date';
@@ -30,11 +28,6 @@ export type Props<F extends BaseFilters> = {
 export const TableFilter = <F extends BaseFilters>({ value: filters, onChange, ...props }: Props<F>) => {
   invariant(filters, '`filters` is required');
   invariant(onChange, '`onChange` is required');
-
-  const [tmpDateRangeValue, setTmpDateRangeValue] = useState([
-    parseDate(filters[`${props.name}From` as keyof F] as string),
-    parseDate(filters[`${props.name}To` as keyof F] as string),
-  ] as DatePickerValue<'range'>);
 
   if (props.type === 'text') {
     return (
@@ -109,17 +102,16 @@ export const TableFilter = <F extends BaseFilters>({ value: filters, onChange, .
       <DatePickerInput
         clearable
         allowSingleDateInRange
-        value={tmpDateRangeValue}
-        onChange={([from, to]) => {
-          setTmpDateRangeValue([from, to]);
-
-          if (from && to) {
-            onChange({
-              [`${props.name}From`]: dayjs(from).startOf('day').toISOString(),
-              [`${props.name}To`]: dayjs(to).endOf('day').toISOString(),
-            } as Partial<F>);
-          }
-        }}
+        value={[
+          parseDate(filters[`${props.name}From` as keyof F] as string),
+          parseDate(filters[`${props.name}To` as keyof F] as string),
+        ]}
+        onChange={([from, to]) =>
+          onChange({
+            [`${props.name}From`]: from ? dayjs(from).startOf('day').toISOString() : null,
+            [`${props.name}To`]: to ? dayjs(to).endOf('day').toISOString() : null,
+          } as Partial<F>)
+        }
         {...props}
         type="range"
       />
