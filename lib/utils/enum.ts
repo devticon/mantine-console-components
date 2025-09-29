4c6f0e4f3f8de05218445ab0f893482c1ade1c6f
Dariusz Syncerek <dsyncerek@outlook.com>
2025-09-29 19:24:29 +0200

    update to react-router v8, mantine v8

--- lib/utils/enum.ts
+++ lib/utils/enum.ts
@@ -1,6 +1,6 @@
 import type { ComboboxData } from '@mantine/core';
 import type { TFunction } from 'i18next';
 
-export function mapEnumToComboboxData(enumObject: object, key: string, t: TFunction): ComboboxData {
+export function mapEnumToComboboxData(enumObject: object, key: string, t: TFunction<string>): ComboboxData {
   return Object.values(enumObject).map(value => ({ value, label: t(`${key}.${value}`) }));
 }
