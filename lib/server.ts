8191ab27adc473f8f9ccc398a95243da4c60fc8f
Dariusz Syncerek <dsyncerek@outlook.com>
2025-12-05 16:52:50 +0100

    add utility for managing cookie-based filter state

--- lib/server.ts
+++ lib/server.ts
@@ -2,3 +2,4 @@ export * from './utils/session-context.js';
 export * from './utils/auth.js';
 export * from './utils/errors.js';
 export * from './utils/i18n.js';
+export * from './utils/cookie-filters.js';
---
+++ lib/utils/cookie-filters.ts
@@ -0,0 +1,20 @@
+import { createCookie } from 'react-router';
+import { getRequest } from './session-context.js';
+
+export async function getCookieFilters<T extends object>(filters: T, key: string) {
+  const cookie = createCookie('filters');
+  const request = getRequest();
+  const cookieHeader = request.headers.get('Cookie');
+  const savedFilters: Record<string, object> = (await cookie.parse(cookieHeader)) || {};
+  const searchParams = new URL(request.url).searchParams;
+  const hasSearchParams = Array.from(searchParams.keys()).length > 0;
+
+  if (hasSearchParams) {
+    savedFilters[key] = filters;
+  }
+
+  return {
+    activeFilters: savedFilters[key] as T,
+    headers: { 'Set-Cookie': await cookie.serialize(savedFilters) },
+  };
+}
